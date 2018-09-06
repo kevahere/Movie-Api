05323a6f355c6cc25cea63241d507c9efde31574
kevahere <kevahere@gmail.com>
2018-09-06 10:46:57 +0300

    removed space from password

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_PORT =587
     MAIL_USE_TLS =True
     MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD =os.environ.get(" MAIL_PASSWORD")
+    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
 
     @staticmethod
     def init_app(app):
